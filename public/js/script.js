413994b32666b0ccc40278ee80a7ba4e089daf7e
guachochi <davis.bradleyj@gmail.com>
2020-05-14 00:20:34 -0700

    modify spacing of Skills

--- public/js/script.js
+++ public/js/script.js
@@ -25,4 +25,15 @@ $(document).ready(function () {
         <p>&bull;${data[0].tech}</p>`
         )
     })
+    $.get(Projects, function(data) {
+        $("#tech").append(
+            `<div class="box">
+            <a class="subtitle has-text-info" href="${data[i].href}" target="_new">${data[i].title}.</a>
+            <hr/>
+            <img src="../images/batman_readme.gif" alt="Gif of B.A.T.M.a.N.">
+            <p class="has-text-link">BATMaN is a community hub and toolset for outdoor enthusiasts to discover new communities and new adventures in the Bay Area and beyond.  Pick a trail, build a community, get outside!</p>
+            <a class="fa fa-github" style="font-size:18px" href="https://github.com/davisbradleyj/ucb-batman" target="_new"> - Github Repo</a>
+            <p class="has-text-link">May 2020</p>`
+    })
 })
+
